e8e43066dba7509a604aa5bb92932ba715f57664
Fabián Araya Ortega <35875278+Mashtic@users.noreply.github.com>
2023-04-27 20:19:50 -0600

    Avance Balanceador

--- Armazon/balanceador.h
+++ Armazon/balanceador.h
@@ -75,18 +75,18 @@ public:
                                 }
                                 if(faltaA==true){
                                     if(colaFabricaA->size()<=colaFabricaComodin->size()){
-                                       colaFabricaA->enqueue(pedidoB);
+                                        colaFabricaA->enqueue(pedidoB);
                                     }else{
-                                       colaFabricaComodin->enqueue(pedidoB);
+                                        colaFabricaComodin->enqueue(pedidoB);
                                     }
                                 }
                                 else{
                                     if (faltaB==true){
-                                       if(colaFabricaB->size()<=colaFabricaComodin->size()){
+                                        if(colaFabricaB->size()<=colaFabricaComodin->size()){
                                             colaFabricaB->enqueue(pedidoB);
-                                       }else{
+                                        }else{
                                             colaFabricaComodin->enqueue(pedidoB);
-                                       }
+                                        }
                                     }
                                     else{
                                         if(faltaC==true){
@@ -99,27 +99,73 @@ public:
 
                         }
                     }
-                }
+                    else{
+                        Articulo* tmp=NULL;
+                        QList<QString> listaFaltantes;
+                        bool faltaA=false;
+                        bool faltaB=false;
+                        bool faltaC=false;
+                        for(int y=0;y<pedidoB.articulos->cantidad;y++) {
+                            tmp=pedidoB.articulos->devuelveArticuloPos(y);
 
+                            if (listaInventario->devuelveArticulo(tmp->codigo)->cantidad-tmp->cantidad<0){
+                                listaFaltantes.append(tmp->codigo);
+                                listaInventario->devuelveArticulo(tmp->codigo)->cantidad=0;
+                            }
+                            else{
+                                listaInventario->devuelveArticulo(tmp->codigo)->cantidad=listaInventario->devuelveArticulo(tmp->codigo)->cantidad-tmp->cantidad;
+                            }
+                            if (listaFaltantes.empty()==true){
+                                colaAlisto->enqueue(pedidoB);
+                                colaPedido->removeAt(i);
+                            }
+                            else{
+                                for(int x=0;x<listaFaltantes.size();x++){
+                                    if (listaInventario->devuelveArticulo(listaFaltantes.at(x))->categoria=="A"){
+                                        faltaA=true;
+                                    }
+                                    else{
+                                        if(listaInventario->devuelveArticulo(listaFaltantes.at(x))->categoria=="B"){
+                                            faltaB=true;
+                                        }
+                                        else{
+                                            if (listaInventario->devuelveArticulo(listaFaltantes.at(x))->categoria=="C"){
+                                                faltaC=true;
+                                            }
+                                        }
+                                    }
+                                }
+                                if(faltaA==true){
+                                    if(colaFabricaA->size()<=colaFabricaComodin->size()){
+                                        colaFabricaA->enqueue(pedidoB);
+                                    }else{
+                                        colaFabricaComodin->enqueue(pedidoB);
+                                    }
+                                }
+                                else{
+                                    if (faltaB==true){
+                                        if(colaFabricaB->size()<=colaFabricaComodin->size()){
+                                            colaFabricaB->enqueue(pedidoB);
+                                        }else{
+                                            colaFabricaComodin->enqueue(pedidoB);
+                                        }
+                                    }
+                                    else{
+                                        if(faltaC==true){
+                                            colaFabricaC->enqueue(pedidoB);
+                                        }
+                                    }
+                                }
+                                colaPedido->removeAt(i);
+                            }
 
+                        }
 
+                    }
 
-
-S
-
-
-
-
-
-                Pedido*  = queue->dequeue();
-                label->setText(queue->_toString());
-
-                while (waitTime > 0)
-                {
-                    attendingLabel->setText(QString::number(waitTime--));
-                    QThread::sleep(1);
                 }
             }
+
 
             QThread::sleep(1);
         }
